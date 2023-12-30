65c7171619b4c0ae57d1b8d5b03174cc5f1b391d
Rasen Nikov <rasen800@gmail.com>
2023-12-30 15:01:41 +0600

    Создал новое представление  ProductList

--- project/simpleapp/views.py
+++ project/simpleapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Category, Product
 
-# Create your views here.
+
+class ProductList(ListView):
+    model = Product    # Указываем модель, объекты которой мы будем выводить
+    ordering = 'name'  # Поле, которое будет использоваться для сортировки объектов
+    template_name = 'products.html'  # Указываем имя шаблона
+    context_object_name = 'products' # Это имя списка, в котором будут лежать все объекты.
+    # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
+
+
