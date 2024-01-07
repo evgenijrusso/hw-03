bd9a5d4939fa097f032737585516a5f2001fc9ba
Rasen Nikov <rasen800@gmail.com>
2024-01-07 11:51:56 +0600

    Обновление модели

--- project/simpleapp/models.py
+++ project/simpleapp/models.py
@@ -32,4 +32,4 @@ class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
-        return f'self.name'
+        return f'{self.name.title()}'
