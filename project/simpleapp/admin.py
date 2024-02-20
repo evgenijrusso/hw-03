53570bb0521c0862b13414b029e8951671e7acbf
Rasen Nikov <rasen800@gmail.com>
2024-02-20 11:59:42 +0600

    Обновление админки

--- project/simpleapp/admin.py
+++ project/simpleapp/admin.py
@@ -1,7 +1,27 @@
 from django.contrib import admin
 from .models import Category, Product, Material, ProductMaterial
 
+# list_display — это список или кортеж со всеми полями, которые Вы хотите видеть в таблице с товарами
+
+# все аргументы уже должны быть вам знакомы, самые нужные из них это request — объект хранящий
+# информацию о запросе и queryset — грубо говоря набор объектов, которых мы выделили галочками.
+def nullfy_quantity(ModelAdmin, request, queryset):
+    queryset.update(quantity=0)
+
+nullfy_quantity.short_description = 'Обнулить товары'
+# описание для более понятного представления в админ панеле задаётся, как будто это объект
+
+
+class ProductAdmin(admin.ModelAdmin):  # создаём новый класс для представления товаров в админке
+   #  list_display = [field.name for field in Product._meta.get_fields()]
+
+    list_display = ('name', 'description', 'quantity', 'price', 'on_stock')
+    list_filter = ('price', 'quantity', 'name')  # добавляем примитивные фильтры в нашу админку
+    search_fields = ('name', 'category__name')  # тут всё очень похоже на фильтры из запросов в базу
+    actions = [nullfy_quantity]  # добавляем действия в список
+
+
 admin.site.register(Category)
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Material)
 admin.site.register(ProductMaterial)
--- project/simpleapp/models.py
+++ project/simpleapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator
 from django.urls import reverse
+from django.core.cache import cache
 
 
 class Product(models.Model):
@@ -12,20 +13,31 @@ class Product(models.Model):
     category = models.ForeignKey(to='Category', on_delete=models.CASCADE, related_name='products')
     price = models.FloatField(validators=[MinValueValidator(0.0)],)
 
-    def __str__(self):
-        return f'{self.name.title()} {self.description[:30]}'
+    # допишем свойство, которое будет отображать, есть ли товар на складе
+    @property
+    def on_stock(self):
+        return self.quantity > 0
 
-    def get_absolute_url(self):
+    def __str__(self):
+        return f'{self.name} {self.quantity}'
+
+    def get_absolute_url(self): # добавим абсолютный путь, чтобы после создания нас перебрасывало на страницу с товаром
         return reverse('product_detail', args=[str(self.id)])
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        cache.delete(f'product-{self.pk}')
+        # затем удаляем его из кэша, чтобы сбросить его
 
 
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True) # Категория, к которой будет привязываться товар
 
     def __str__(self):
-        return f'{self.name.title()}'
+        return f'{self.name}'
 
 # --------------------------------------------------
+
 
 class Material(models.Model):
     name = models.CharField(max_length=100)
